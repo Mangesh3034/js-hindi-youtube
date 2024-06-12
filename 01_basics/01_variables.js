1965dfbe4e2c2f036357b3ac60716e7e7ebfd028
Mangesh3034 <148550768+Mangesh3034@users.noreply.github.com>
2024-06-12 14:40:34 +0000

    second commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=144553
+let accountEmail="mangesh.gmail.com"
+var  accountPassword="12345"
+accountCity="Jaipur"
+//what if you declare a variable without initializing it? then it 
+let accountState;
+//to print all variables at once
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+let name="Mangesh" //string
+let age=28 //integer
+let isLoggedIn=true // boolean
+console.log(typeof undefined);
+console.log(typeof null);
